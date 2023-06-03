f0eca9257dbf3d29777c4a7faf4bc99401a131aa
NikolayWw <0965175291asd@gmail.com>
2023-06-03 12:18:13 +0300

    main

--- Assets/CodeBase/Extension/DataExtension.cs
+++ Assets/CodeBase/Extension/DataExtension.cs
@@ -2,9 +2,9 @@
 {
     public static class DataExtension
     {
-        public static void ArrayBubbleSort<TSort>(this TSort[] array) where TSort : IArraySort
+        public static void ArrayBubbleSort(this IArraySort[] array)
         {
-            TSort temp;
+            IArraySort temp;
             for (int i = 0; i < array.Length; i++)
                 for (int j = 0; j < array.Length - 1 - i; j++)
                     if (array[j].SortNumber > array[j + 1].SortNumber)
